24bc9d9d650c43273e7aadf51036ded19398df38
Edgar Duran <edgarduran@Edgars-MacBook-Pro.local>
2015-10-14 22:11:12 -0600

    still working

--- app/models/skill_inventory.rb
+++ app/models/skill_inventory.rb
@@ -1,5 +1,57 @@
+require 'yaml/store'
+
 class SkillInventory
+  def self.create(skill)
+    database.transaction do
+      database['skills'] ||= []
+      database['total'] ||= 0
+      database['total'] += 1
+      database['skills'] << { "id" => database['total'], "title" => skill[:title], "description" => skill[:description] }
+    end
+  end
+
+  def self.update(id, data)
+    database.transaction do
+      target = database['skills'].find { |skill| skill["id"] == id }
+      target["title"] = data[:title]
+      target["description"] = data[:description]
+    end
+  end
+
+  def self.delete(id)
+    database.transaction do
+      database['skills'].delete_if { |skill| skill['id'] == id }
+    end
+  end
   def self.database
-    @database ||= YAML::Store.new("db/skill_inventory")
+    if ENV["RACK_ENV"] == 'test'
+      @database ||= YAML::Store.new("db/skill_inventory_test")
+    else
+      @database ||= YAML::Store.new("db/skill_inventory")
+  end
+
+  def self.raw_skills
+    database.transaction do
+      database['skills'] || []
+    end
+  end
+
+  def self.all
+    raw_skills.map { |data| Skill.new(data) }
+  end
+
+  def self.raw_skill(id)
+    raw_skills.find { |skill| skill["id"] == id }
+  end
+
+  def self.find(id)
+    Skill.new(raw_skill(id))
+  end
+
+  def self.delete_all
+    database.transaction do
+      database['skills'] = []
+      database['total'] = 0
+    end
   end
 end
